c0ec41c41344ec2b64ea0a3ea11cbbb21f501937
fAEDKAN <103541453+fAEDKAN@users.noreply.github.com>
2023-07-24 08:14:11 -0300

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "./layout";
+import AppRoutes from "./routes";
+
+function App() {
+  return (
+    <MainLayout>
+      <AppRoutes/>
+    </MainLayout>
+  );
+}
+
+export default App;
